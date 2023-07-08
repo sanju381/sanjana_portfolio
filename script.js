236508286a316c4f6b645367b25080adf6467ec4
sanju381 <107574394+sanju381@users.noreply.github.com>
2023-07-08 12:24:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav' a);
+
+window.onscroll=()=>{
+    sections.forEach
+    let header=document.querySelector('header');
+    header.classList.toggle('sticky',window.scrolly>100);
+}
